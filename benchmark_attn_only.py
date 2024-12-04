b6b32b718f23a2ccd208cc03df2902f505e72b02
spikerheado1234 <ahangupta.96@gmail.com>
2024-12-04 02:03:06 -0600

    add new comment

--- benchmark_attn_only.py
+++ benchmark_attn_only.py
@@ -118,7 +118,8 @@ if __name__ == '__main__':
     #model_config = get_config_for_2b_v2()
     ## Make 32 bit precision and turn of logit softcapping.
     tknizer = tokenizer.Tokenizer(model_config.tokenizer)
-    #model_config.dtype = 'float32'
+    
+    ## Currently we ONLY support bfloat16.
     model_config.quant = False
     model_config.dtype = 'bfloat16'
     model_config.attn_logit_softcapping = None
